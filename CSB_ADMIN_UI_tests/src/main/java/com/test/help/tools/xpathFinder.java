6ea7af46a12089f58005cbc5bb3b2d65b4f3a600
tymtsiv <tymtsiv.n@gmail.com>
2012-10-19 12:16:21 +0300

    '-'

--- CSB_ADMIN_UI_tests/src/main/java/com/test/help/tools/xpathFinder.java
+++ CSB_ADMIN_UI_tests/src/main/java/com/test/help/tools/xpathFinder.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.WebDriver;
 public class xpathFinder {
 public void findXpathByName(WebDriver driver){
     driver.findElements(By.className("pull-right"));
-    System.out.println(driver);
+    System.out.println(driver.findElements(By.className("pull-right")));
 
 }
 }
